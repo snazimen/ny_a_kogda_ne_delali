ba4097a7152cbd5320d0f9f09a7bbc89dee012e3
Danil <snazimen@yandex.ru>
2024-11-14 12:26:12 +0300

    add_server

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("error loading .env file: %s", err)
+	}
 
-	fmt.Println("Server is listening...")
-	http.ListenAndServe(":8888", http.FileServer(http.Dir("web")))
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8888"
+	}
+
+	webDir := "./web"
+	r := chi.NewRouter()
+	r.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	serverAdress := fmt.Sprintf("localhost:%s", port)
+	log.Println("listen on " + serverAdress)
+	if err = http.ListenAndServe(serverAdress, http.FileServer(http.Dir(webDir))); err != nil {
+		log.Panicf("start server error:%s", err.Error())
+	}
 }
